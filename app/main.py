bcdebcbabb595b269c7b1b8da2a1e3d8893d5e63
重霖 黄 <3564567691@qq.com>
2026-07-31 17:08:49 +0800

    更新用户信息

--- app/main.py
+++ app/main.py
@@ -34,3 +34,29 @@ def add_user(username: str, password: str, db : Session = Depends(get_db)):  # n
         "username":user.username,
         "create_time":user.create_time
     }
+#查询用户
+@app.get("/get_user/{id}")
+def get_user(id: int , db : Session = Depends(get_db)):  # noqa: B008
+    user = db.quary(User).filter(User.id == id).first()
+    if not User:
+        raise ValueError("用户不存在")
+    return {
+        "id":user.id,
+        "username":user.username,
+        "create_time":user.create_time
+    }
+
+#更改用户信息
+@app.patch("/user/{id}")
+def update_user(user_id:int,update_data: UserUpdateDTO,db: Session = Depends(get_db)):  # noqa: B008
+    #检测用户是否存在
+    if not user :
+        raise ValueError("用户不存在")
+
+    date_dict = update_data.model_dump(exclude_unset=True)
+
+    for key,value in date_dict.items():
+        setattr(user,key,value)
+    db.commit()
+    db.refresh(user)
+    return user

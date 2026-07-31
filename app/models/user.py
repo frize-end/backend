846743f0ee2608e394967c5648ff7e551d3ae77f
重霖 黄 <3564567691@qq.com>
2026-07-31 10:24:00 +0800

    修改重复定义的Base基类

--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,13 @@
 from datetime import datetime
 
 from sqlalchemy import Column, Datetime, Integer, String
-from sqlalchemy.orm import DeclarativeBase
 
+from app.core.database import Base
 
-class Base(DeclarativeBase):
-    pass
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primarykey=True, index=True)
-    username = Column(String(10),nullable=False,index=True)
-    password = Column(String(20),nullable=False)
-    datetime = Column(Datetime,default=datetime.now,comment="创建时间")
+    id = Column(Integer, primarykey=True)
+    username = Column(String(32),nullable=False,index=True)
+    password = Column(String(128),nullable=False)
+    creat_time = Column(Datetime,default=datetime.now,comment="创建时间")

fd77108af862abe05351a894f185f574a5ad9dda
重霖 黄 <3564567691@qq.com>
2026-07-31 12:12:11 +0800

    ORM自动建表

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Datetime, Integer, String
+from sqlalchemy import Column, DateTime, Integer, String
 
 from app.core.database import Base
 
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primarykey=True)
-    username = Column(String(32),nullable=False,index=True)
-    password = Column(String(128),nullable=False)
-    creat_time = Column(Datetime,default=datetime.now,comment="创建时间")
+    id = Column(Integer, primary_key=True, comment="用户id")
+    username = Column(String(32),nullable=False,index=True, comment="用户名")
+    password = Column(String(128),nullable=False, comment="密码")
+    creat_time = Column(DateTime,default=datetime.now,comment="创建时间")

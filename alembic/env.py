4878ed349f9641640715bf3c166b668849f7de36
重霖 黄 <3564567691@qq.com>
2026-08-06 11:19:02 +0800

    重构数据库迁移脚本，删除旧的用户表迁移文件并添加新的用户表迁移文件，包含超级用户列

--- alembic/env.py
+++ alembic/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine, pool
 
 from alembic import context
 from app.core.database import DB_URL, Base
+from app.models.user import User  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.

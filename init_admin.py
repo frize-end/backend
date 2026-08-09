cb540da025790da2379d75aa4af1bb6cd748f192
重霖 黄 <3564567691@qq.com>
2026-08-09 23:52:49 +0800

    添加创建管理员功能，检查管理员是否已存在并进行相应处理

--- init_admin.py
+++ init_admin.py
@@ -1,3 +1,20 @@
 from app.core.database import SessionLocal
 from app.core.security import hash_password
 from app.models.user import User
+
+def create_admin():
+    db = SessionLocal()
+    try:
+        # 检查管理员是否已存在
+        admin = db.query(User).filter(User.username == "admin", User.is_delete == 0).first()
+        if admin:
+            print("管理员已存在")
+            return
+
+        admin = User(username="admin",
+                      password=hash_password("admin123"),
+                      phone="13210533587",
+                      role="admin",
+                      is_delete=0
+                      )
+        db.add(admin)
